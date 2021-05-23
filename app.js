21c1676c2bd7eb4e96cae1cc4e031b7d50183b04
Max <max.work.dev@gmail.com>
2021-05-23 22:27:59 +0300

    fix(app): not require middleware

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 const db = require("./db");
 const user = require("./controllers/usercontroller");
 const game = require("./controllers/gamecontroller");
+const validateSession = require("./middleware/validate-session");
 
 const PORT = process.env.PORT || 4000;
 
 db.sync();
 app.use(express.json());
 app.use("/api/auth", user);
-app.use(require("./middleware/validate-session"));
-app.use("/api/game", game);
+app.use("/api/game", validateSession, game);
 app.listen(PORT, function () {
   console.log(`App is listening on ${PORT}`);
 });
