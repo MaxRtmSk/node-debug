e0a9749db7696e9e93012f22dc0e20ccbbfb4be9
Max <max.work.dev@gmail.com>
2021-05-23 23:39:38 +0300

    fix: change name const &

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const db = require("./db");
-const user = require("./controllers/usercontroller");
-const game = require("./controllers/gamecontroller");
+const usercontroller = require("./controllers/usercontroller");
+const gamecontroller = require("./controllers/gamecontroller");
 const validateSession = require("./middleware/validate-session");
 
 const PORT = process.env.PORT || 4000;
@@ -10,8 +10,8 @@ const app = express();
 
 db.sync();
 app.use(express.json());
-app.use("/api/auth", user);
-app.use("/api/game", validateSession, game);
+app.use("/api/auth", usercontroller);
+app.use("/api/game", validateSession, gamecontroller);
 app.listen(PORT, function () {
   console.log(`App is listening on ${PORT}`);
 });
